34b8f93ec866c925ca81662e0190cfb42b9733e6
ByteBlender <byteblender21@gmail.com>
2023-07-01 17:08:45 +0200

    Implement first test

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    run()?.await
 }
